88439c8911ef4b971ee53887f8d6324102102ef1
victorf <mail@victorf.ch>
2020-04-12 11:13:56 +0000

    created internal function for loop server list

--- hackapoll.sh
+++ hackapoll.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
+loop_server_list()
+{
+    LCOUNTRIES=$1
+    LMAXSVR=$2
+    LURI=$3
+    LPARAMS=$4
+
+    for run in $LCOUNTRIES
+    do
+        for (( svr=2; svr <= $LMAXSVR; ++svr ))
+        do
+            printf "\ntrying server $run#$svr..."
+
+            if sudo protonvpn c "$run#$svr" -p tcp | grep "Connected!"
+            then
+                printf "\nsending POST request...\n"
+                curl -d "${LPARAMS}" "${LURI}"
+                printf "\ndisconnecting...\n"
+                sudo protonvpn disconnect
+            else
+                printf "\nconnection server $run#$svr failed or does not exist.\n"
+            fi
+
+        done
+    done
+}
+
 COUNTRIES="AR AU AT BE BR BG BF CA CZ CR DK EE FI FR DE GR HK IS IN IE IL IT JP JP-FREE KR LV LT LU MD MX"
 COUNTRIES="${COUNTRIES} NL NL-FREE NZ NO PL PT RO RU RS SG SK ZA ES SE CH TW TR UA AE UK US-FREE US-CA US-FL"
 COUNTRIES="${COUNTRIES} US-GA US-IL US-NJ US-NY US-TX US-UT US-VA US-GA"
@@ -13,21 +40,6 @@ read URI
 printf "\nenter params:\n"
 read PARAMS
 
-for run in $COUNTRIES
-do
-    for svr in {1..120}
-    do
-        printf "\ntrying server $run#$svr...\n"
+MAXSVR=120
 
-        if sudo protonvpn c "$run#$svr" -p tcp | grep "Connected!"
-        then
-            printf "\nsending POST request...\n"
-            curl -d "${PARAMS}" "${URI}"
-            printf "\ndisconnecting...\n"
-            sudo protonvpn disconnect
-        else
-            printf "\nconnection server $run#$svr failed or does not exist.\n"
-        fi
-
-    done
-done
+loop_server_list "${COUNTRIES}" $MAXSVR "${URI}" "${PARAMS}"
