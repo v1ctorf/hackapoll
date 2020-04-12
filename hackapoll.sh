989ae1652de6cd20c4e3d9aa09a0bd1e8702ec8a
victorf <mail@victorf.ch>
2020-04-12 14:02:39 +0000

    replaced head by awk when reading line from params file

--- hackapoll.sh
+++ hackapoll.sh
@@ -2,7 +2,8 @@
 
 get_params()
 {
-    echo $(head -n 1 params)
+    LINE=$1
+    echo $(awk NR==$LINE params)
 }
 
 loop_server_list()
@@ -10,34 +11,34 @@ loop_server_list()
     LCOUNTRIES=$1
     LMAXSVR=$2
     LURI=$3
-    LPARAMS=$(get_params)
 
     for run in $LCOUNTRIES
     do
         for (( svr=1; svr <= $LMAXSVR; ++svr ))
         do
-            printf "\ntrying server $run#$svr... "
+            printf "\ntrying server $run#$svr...\n"
 
             if sudo protonvpn c "$run#$svr" -p tcp | grep "Connected!"
             then
+                LPARAMS=$(get_params 1)
                 printf "\nsending POST request with params:\n=> ${LPARAMS}\n"
                 curl -d "${LPARAMS}" "${LURI}" -m 30.0 --connect-timeout 30.0
                 printf "\ndisconnecting...\n"
                 sudo protonvpn disconnect
             else
-                printf "\nconnection server $run#$svr failed or does not exist.\n"
+                printf "connection server $run#$svr failed or does not exist.\n"
             fi
 
         done
     done
 }
 
-COUNTRIES="AR AU AT BE BR BG BF CA CZ CR DK EE FI FR DE GR HK IS IN IE IL IT JP JP-FREE KR LV LT LU MD MX"
+COUNTRIES="BG AR AU AT BE BR BF CA CZ CR DK EE FI FR DE GR HK IS IN IE IL IT JP JP-FREE KR LV LT LU MD MX"
 COUNTRIES="${COUNTRIES} NL-FREE NZ NO PL PT RO RU RS SG SK ZA ES SE CH TW TR UA AE UK US-FREE"
 COUNTRIES_XL="NL US-CA US-FL US-GA US-IL US-NJ US-NY US-TX US-UT US-VA US-GA"
 
 printf "\nenter endpoint:\n"
 read URI
 
-loop_server_list "${COUNTRIES_XL}" 150 "${URI}"
 loop_server_list "${COUNTRIES}" 40 "${URI}"
+loop_server_list "${COUNTRIES_XL}" 150 "${URI}"
